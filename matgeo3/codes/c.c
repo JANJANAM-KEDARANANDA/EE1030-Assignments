0cb17d1615c6c87208d173fa23c1f811b2a8c38f
JANJANAM-KEDARANANDA <kedaranandajanjanam@gmail.com>
2024-09-03 00:24:55 +0530

    Hello

--- matgeo3/codes/c.c
+++ matgeo3/codes/c.c
@@ -1,30 +1,81 @@
 #include <stdio.h>
-double det(double a, double b, 
-                       double d, double e, 
-                       double g, double h) {
-    return a * (e * 1 - 1 * h) 
-         - b * (d * 1 - 1 * g) 
-         + 1 * (d * h - e * g);
-}
-int main() {
-    double a, b;
-    double d, e;
-    double g, h;
-    printf("enter the x coordinate of 1st point : ");
-    scanf("%lf",&a);
-    printf("enter the y coordinate of 1st point : ");
-    scanf("%lf",&b);
-    printf("enter the x coordinate of 2nd point : ");
-    scanf("%lf",&d);
-    printf("enter the y coordinate of 2nd point : ");
-    scanf("%lf",&e);
-    printf("enter the x coordinate of 3rd point : ");
-    scanf("%lf",&g);
-    printf("enter the y coordinate of 3rd point : ");
-    scanf("%lf",&h);
-    double k = det(a, b,d, e, g, h);
-    printf("The determinant of the matrix is : %.2f\n", k);
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-    return 0;
+#include "libs/matfun.h"
+#include "libs/geofun.h"
+
+
+// Function to calculate the determinant of a matrix given by three points
+double det(const double p1[], const double p2[], const double p3[]) {
+    return p1[0] * (p2[1] * 1 - 1 * p3[1])
+         - p1[1] * (p2[0] * 1 - 1 * p3[0])
+         + 1 * (p2[0] * p3[1] - p2[1] * p3[0]);
+}
+
+int main() {
+    double point1[2], point2[2], point3[2]; //using arrays
+
+    // Input for the first point
+    printf("Enter the x coordinate of 1st point: ");
+    scanf("%lf", &point1[0]);
+    printf("Enter the y coordinate of 1st point: ");
+    scanf("%lf", &point1[1]);
+
+    // Input for the second point
+    printf("Enter the x coordinate of 2nd point: ");
+    scanf("%lf", &point2[0]);
+    printf("Enter the y coordinate of 2nd point: ");
+    scanf("%lf", &point2[1]);
+
+    // Input for the third point
+    printf("Enter the x coordinate of 3rd point: ");
+    scanf("%lf", &point3[0]);
+    printf("Enter the y coordinate of 3rd point: ");
+    scanf("%lf", &point3[1]);
+
+    // Calculate the determinant using the points
+    double k = det(point1, point2, point3);
+
+    // Output the result
+    printf("The determinant of the matrix is: %.2f\n", k);
+
+    
+    
+    	double **A,**B,**C;
+	A = createMat(2,1);
+	B = createMat(2,1);
+	C = createMat(2,1);
+	A[0][0] = point1[0];
+	A[1][0] = point1[1];
+	B[0][0] = point2[0];
+	B[1][0] = point2[1];
+        C[0][0] = point3[0];
+        C[1][0] = point3[1];
+	//dat file code begins
+	FILE *file;
+	file = fopen("values.dat", "w");
+
+	if (file == NULL) {
+		printf("Error opening file!\n");
+		return 1;
+	}
+	fprintf(file, "x y\n");
+	fprintf(file, "%.02f %.02f\n", A[0][0],A[1][0]);
+	fprintf(file, "%.02f %.02f\n", B[0][0],B[1][0]);
+	fprintf(file, "%.02f %.02f\n", C[0][0],C[1][0]);
+
+	fclose(file);
+	// end
+	printf("Results have been written to values.dat\n");
+
+	freeMat(A,2);
+	freeMat(B,2);
+	freeMat(C,2);
+	return 0;
 }
 
--- matgeo3/codes/p.py
+++ matgeo3/codes/p.py
@@ -1,24 +1,4 @@
 import ctypes
-
-# Load the shared library
-lib = ctypes.CDLL('./det.so')
-
-# Define the argument and return types of the C function
-lib.det.argtypes = [ctypes.c_double, ctypes.c_double,
-                                ctypes.c_double, ctypes.c_double,
-                                ctypes.c_double, ctypes.c_double]
-lib.det.restype = ctypes.c_double
-
-# Define a function to call the C function and get the determinant
-def get_determinant(a, b, d, e, g, h):
-    return lib.det(a, b,  d, e,  g, h)
-
-# Example usage
-det = get_determinant(2,0,1,2,0,4)
-print(f"The determinant of the matrix is: {det:.2f}")
-
-
-
 import sys                                          #for path to external scripts
 sys.path.insert(0, '/home/kedar/Documents/matgeo/codes/CoordGeo')        #path to my scripts
 import numpy as np
@@ -31,13 +11,42 @@ from line.funcs import *
 from triangle.funcs import *
 from conics.funcs import circ_gen
 
+lib = ctypes.CDLL('./det.so')
+
+# Define the argument and return types of the `det` function
+lib.det.argtypes = (ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double))
+lib.det.restype = ctypes.c_double
+
+def pdet(p1, p2, p3):
+    # Convert numpy arrays to ctypes arrays
+    p1_ctypes = p1.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+    p2_ctypes = p2.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+    p3_ctypes = p3.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+    
+    return lib.det(p1_ctypes, p2_ctypes, p3_ctypes)
+
+# Define a function to call the C function and get the determinant
+data = np.genfromtxt('values.dat', delimiter=' ', names=True)
+x = data['x']
+y = data['y']
+p1 = np.array([x[0], y[0],1], dtype=np.float64)
+p2 = np.array([x[1], y[1],1], dtype=np.float64)
+p3 = np.array([x[2], y[2],1], dtype=np.float64)
+
+det = pdet(p1,p2,p3)
+print(f"The determinant of the matrix is: {det:.2f}")
+
+
+
+
+
 
 
 
 #Given points
-A = np.array(([2,0])).reshape(-1,1)
-B = np.array(([1,2])).reshape(-1,1)
-C = np.array(([0,4])).reshape(-1,1)
+A = np.array([x[0],y[0]], dtype=np.float64).reshape(-1,1)
+B = np.array([x[1],y[1]], dtype=np.float64).reshape(-1,1)
+C = np.array([x[2],y[2]], dtype=np.float64).reshape(-1,1)
 #Generating all lines
 x_AC = line_gen(A,C)
 #Plotting all lines
