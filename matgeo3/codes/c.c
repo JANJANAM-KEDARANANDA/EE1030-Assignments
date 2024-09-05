750a6aa8877df71dca8e82c82ac376959116da92
JANJANAM-KEDARANANDA <kedaranandajanjanam@gmail.com>
2024-09-06 01:17:32 +0530

    Hello

--- matgeo3/codes/c.c
+++ matgeo3/codes/c.c
@@ -16,7 +16,16 @@ double det(const double p1[], const double p2[], const double p3[]) {
          - p1[1] * (p2[0] * 1 - 1 * p3[0])
          + 1 * (p2[0] * p3[1] - p2[1] * p3[0]);
 }
-
+float **calcpoints(double **A, double **B, double **AB){
+	AB[0][0]= A[0][0]; AB[1][0]= A[1][0]; AB[2][0] = A[2][0];
+	AB[0][51]= B[0][0]; AB[1][51]= B[1][0]; AB[2][51] = B[2][0];
+        for(int i = 1; i<=50; i+=1){
+          float j = (float) i/50;
+          AB[0][i]= AB[0][0] + j*(AB[0][51]- AB[0][0]);
+          AB[1][i]= AB[1][0] + j*(AB[1][51]- AB[1][0]);
+          AB[2][i]= AB[2][0] + j*(AB[2][51]- AB[2][0]);
+	}
+}
 int main() {
     double point1[2], point2[2], point3[2]; //using arrays
 
@@ -46,17 +55,26 @@ int main() {
 
     
     
-    	double **A,**B,**C;
-	A = createMat(2,1);
-	B = createMat(2,1);
-	C = createMat(2,1);
+    	double **A,**B,**C,**AB,**CA,**BC;
+	A = createMat(3,1);
+	B = createMat(3,1);
+	C = createMat(3,1);
+	AB = createMat(3,52);
+	BC = createMat(3,52);
+	CA = createMat(3,52);
 	A[0][0] = point1[0];
 	A[1][0] = point1[1];
+	A[2][0] = 0;
 	B[0][0] = point2[0];
 	B[1][0] = point2[1];
+	B[2][0] = 0;
         C[0][0] = point3[0];
         C[1][0] = point3[1];
+        C[2][0] = 0;
 	//dat file code begins
+	calcpoints(A,B,AB);
+        calcpoints(B,C,BC);
+	calcpoints(C,A,CA);
 	FILE *file;
 	file = fopen("values.dat", "w");
 
@@ -64,18 +82,17 @@ int main() {
 		printf("Error opening file!\n");
 		return 1;
 	}
-	fprintf(file, "x y\n");
-	fprintf(file, "%.02f %.02f\n", A[0][0],A[1][0]);
-	fprintf(file, "%.02f %.02f\n", B[0][0],B[1][0]);
-	fprintf(file, "%.02f %.02f\n", C[0][0],C[1][0]);
+	fprintf(file, "xab yab zab xbc ybc zbc xca yca zca\n");
+	
+	for(int i=0; i<=51; i+=1){
+  	  fprintf(file, "%.02lf %.02lf %.02lf %.02lf %.02lf %.02lf %.02lf %.02lf %.02lf\n", AB[0][i],AB[1][i],AB[2][i], BC[0][i],BC[1][i],BC[2][i],CA[0][i],CA[1][i],CA[2][i]);
+	}
 
 	fclose(file);
-	// end
 	printf("Results have been written to values.dat\n");
 
-	freeMat(A,2);
-	freeMat(B,2);
-	freeMat(C,2);
+	freeMat(AB,3);
+	freeMat(A,3);
 	return 0;
 }
 
--- matgeo3/codes/p.py
+++ matgeo3/codes/p.py
@@ -1,83 +1,74 @@
-import ctypes
+#Code by GVV Sharma
+#September 12, 2023
+#Revised July 21, 2024
+#released under GNU GPL
+#Point Vectors
+
+
 import sys                                          #for path to external scripts
-sys.path.insert(0, '/home/kedar/Documents/matgeo/codes/CoordGeo')        #path to my scripts
+sys.path.insert(0, '/home/kedar/Documents/matgeo/codes/CoordGeo')        #path to my scripts /home/kedar/EE1030/1-1.6-22/codes/assignment1-1-6-22.c
 import numpy as np
 import numpy.linalg as LA
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from mpl_toolkits.mplot3d import Axes3D
 
 #local imports
 from line.funcs import *
 from triangle.funcs import *
 from conics.funcs import circ_gen
-
-lib = ctypes.CDLL('./det.so')
-
-# Define the argument and return types of the `det` function
-lib.det.argtypes = (ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double))
-lib.det.restype = ctypes.c_double
-
-def pdet(p1, p2, p3):
-    # Convert numpy arrays to ctypes arrays
-    p1_ctypes = p1.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
-    p2_ctypes = p2.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
-    p3_ctypes = p3.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
-    
-    return lib.det(p1_ctypes, p2_ctypes, p3_ctypes)
-
-# Define a function to call the C function and get the determinant
 data = np.genfromtxt('values.dat', delimiter=' ', names=True)
-x = data['x']
-y = data['y']
-p1 = np.array([x[0], y[0],1], dtype=np.float64)
-p2 = np.array([x[1], y[1],1], dtype=np.float64)
-p3 = np.array([x[2], y[2],1], dtype=np.float64)
-
-det = pdet(p1,p2,p3)
-print(f"The determinant of the matrix is: {det:.2f}")
-
-
-
-
-
-
-
-
+xab = data['xab']
+yab = data['yab']
+zab = data['zab']
+xbc = data['xbc']
+ybc = data['ybc']
+zbc = data['zbc']
+xca = data['xca']
+yca = data['yca']
+zca = data['zca']
 #Given points
-A = np.array([x[0],y[0]], dtype=np.float64).reshape(-1,1)
-B = np.array([x[1],y[1]], dtype=np.float64).reshape(-1,1)
-C = np.array([x[2],y[2]], dtype=np.float64).reshape(-1,1)
-#Generating all lines
-x_AC = line_gen(A,C)
-#Plotting all lines
-plt.plot(x_AC[0,:],x_AC[1,:],label='$AC$')
+A = np.array(([xab[0], yab[0],zab[0]])).reshape(-1,1) 
+B = np.array(([xbc[0], ybc[0],zbc[0]])).reshape(-1,1) 
+C = np.array(([xca[0], yca[0],zca[0]])).reshape(-1,1) 
+ABmid = np.vstack(([xab, yab,zab]))
+BCmid = np.vstack(([xbc, ybc,zbc]))
+ACmid = np.vstack(([xca, yca,zca]))
 
-#Labeling the coordinates
-tri_coords = np.block([[A,B,C]])
-plt.scatter(tri_coords[0,:], tri_coords[1,:])
-vert_labels = ['A','B','C']
+# Create a figure and a 3D Axes
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(111, projection='3d')
+
+
+# Scatter plot
+colors = np.arange(1, 4)  # Example colors
+tri_coords = np.block([A, B, C])  # Stack A, B, C vertically
+ax.scatter(tri_coords[0, :], tri_coords[1, :], tri_coords[2, :], c=colors)
+ax.scatter(ACmid[0, :], ACmid[1, :], ACmid[2, :], c = "black")
+vert_labels = ['A', 'B', 'C']
+
 for i, txt in enumerate(vert_labels):
-    #plt.annotate(txt, # this is the text
-    plt.annotate(f'{txt}\n({tri_coords[0,i]:.0f}, {tri_coords[1,i]:.0f})',
-                 (tri_coords[0,i], tri_coords[1,i]), # this is the point to label
-                 textcoords="offset points", # how to position the text
-                 xytext=(20,-10), # distance from text to points (x,y)
-                 ha='center') # horizontal alignment can be left, right or center
-# use set_position
-ax = plt.gca()
-#ax.spines['top'].set_color('none')
-#ax.spines['left'].set_position('zero')
-#ax.spines['right'].set_color('none')
-#ax.spines['bottom'].set_position('zero')
+    # Annotate each point with its label and coordinates
+    ax.text(tri_coords[0, i], tri_coords[1, i], tri_coords[2, i], f'{txt}\n({tri_coords[0, i]:.02f}, {tri_coords[1, i]:.02f}, {tri_coords[2, i]:.02f})',
+             fontsize=12, ha='center', va='bottom')
+
+ax.spines['top'].set_color('none')
+ax.spines['left'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['bottom'].set_position('zero')
+
+'''
 ax.spines['left'].set_visible(False)
 ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
 ax.spines['bottom'].set_visible(False)
-#plt.xlabel('$x$')
-#plt.ylabel('$y$')
+plt.xlabel('$x$')
+plt.ylabel('$y$')
 plt.legend(loc='best')
+'''
+plt.xlabel('$X$-Axis')
+plt.ylabel('$Y$-Axis')
 plt.grid() # minor
 plt.axis('equal')
-
-
+plt.title('Showing that A,B,C are collinear', loc = 'right', pad = 15)
 plt.show()
