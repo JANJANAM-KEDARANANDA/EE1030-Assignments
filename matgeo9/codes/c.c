674b79bac13da23aff7284d41c80357c9afd91b5
JANJANAM-KEDARANANDA <kedaranandajanjanam@gmail.com>
2024-09-24 20:00:28 +0530

    Hello

---
+++ matgeo9/codes/c.c
@@ -0,0 +1,32 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "libs/matfun.h"
+#include "libs/geofun.h"
+int main() {
+	int A=-1,B=1,C=2;
+	double **d,**n;
+	n=createMat(2,1);
+	d=createMat(2,1);
+	n[0][0]=A;
+	n[1][0]=B;
+	d[0][0]=B;
+	d[1][0]=-A;
+	FILE *file;
+	file = fopen("values.dat","w");
+	if (file == NULL) {
+		printf("Error opening file!\n");
+		return 1;
+	}
+	fprintf(file,"d n\n");
+	fprintf(file,"%.2f %.2f\n",d[0][0],n[0][0]);
+	fprintf(file,"%.2f %.2f\n",d[1][0],n[1][0]);
+	fclose(file);
+	freeMat(d,2);
+	freeMat(n,2);
+}	
---
+++ matgeo9/codes/p.py
@@ -0,0 +1,44 @@
+import sys                                          # for path to external scripts
+sys.path.insert(0, '/home/kedar/Documents/matgeo/codes/CoordGeo')  # path to my scripts
+import numpy as np
+import numpy.linalg as LA
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# Define points for the line
+x_values = np.linspace(-5, 10, 100)
+y_values = (x_values - 2)
+#Create a figure
+plt.figure(figsize=(8, 6))
+
+# Plot the line
+plt.plot(x_values, y_values, label='Line: $y = x - 2 $', color='cyan')
+
+# Define the direction vector and normal vector
+data = np.genfromtxt('values.dat', delimiter=' ', names=True)
+x = data['d']
+y = data['n']
+direction_vector = np.array([x[0],x[1]])  
+normal_vector = np.array([y[0], y[1]])
+origin = np.array([2,0])  
+# Plot the direction vector
+plt.quiver(*origin, *direction_vector, angles='xy', scale_units='xy', scale=1, color='green', label='Direction Vector')
+
+# Plot the normal vector
+plt.quiver(*origin, *normal_vector, angles='xy', scale_units='xy', scale=1, color='blue', label='Normal Vector')
+
+# Set limits and labels
+plt.xlim(-5, 10)
+plt.ylim(-5, 10)
+plt.axhline(0, color='black', linewidth=0.5, ls='--')
+plt.axvline(0, color='black', linewidth=0.5, ls='--')
+
+# Set labels and title
+plt.xlabel('$X$-Axis')
+plt.ylabel('$Y$-Axis')
+plt.title('Plot of the Line: $y = x - 2$ with Direction and Normal Vectors')
+plt.grid(True)
+plt.legend()
+
+# Show the plot
+plt.show()
